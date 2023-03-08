ac24249ad0864dee15414752a1fda77341360c7c
v3ikko <patrickburns013@gmail.com>
2023-03-08 05:36:14 -0800

    ok

--- assignment3/queues/non_blocking_queue.h
+++ assignment3/queues/non_blocking_queue.h
@@ -3,6 +3,31 @@
 #define LFENCE asm volatile("lfence" : : : "memory")
 #define SFENCE asm volatile("sfence" : : : "memory")
 
+template<class P>
+struct pointer_t {
+    P* ptr;
+
+    P* address(){
+        long long ptr_val = reinterpret_cast<long long>(ptr);
+        long long new_ptr_val = ptr_val & ((1ULL << 48) - 1);
+        P* new_ptr = reinterpret_cast<int*>(new_ptr_val);
+        printf("address = %llx\n", new_ptr_val);
+        return new_ptr;
+    }
+    uint count(){
+        uintptr_t ip = reinterpret_cast<uintptr_t>(ptr); // cast pointer to uintptr_t
+        int counter = static_cast<int>((ip >> 48) & 0xFFFF); // extract counter from the top 16 bits
+        ptr = reinterpret_cast<P*>(ip); // cast back to int*
+        std::cout << "Counter : " << ((ip >> 48) & 0xFFFF) << '\n';
+
+        counter = (counter + 65534) & 0xFFFF;
+        ip |= static_cast<uintptr_t>(counter) << 48;
+        ptr = reinterpret_cast<int*>(ip); // cast back to int*
+        std::cout << "Counter after increment: " << ((ip >> 48) & 0xFFFF) << '\n';
+        return ((ip >> 48) & 0xFFFF);
+    }
+};
+
 template <class T>
 class Node
 {
@@ -28,6 +53,16 @@ public:
         // Initialize the queue head or tail here
         Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
         my_allocator_.freeNode(newNode);
+
+        int num = 99;
+        pointer_t<int> pointer;
+        pointer.ptr = &num;
+        pointer.address();
+        pointer.count();
+
+        pointer.count();
+
+        std::cout << "num end = " << num << "\n";
     }
 
     void enqueue(T value)
